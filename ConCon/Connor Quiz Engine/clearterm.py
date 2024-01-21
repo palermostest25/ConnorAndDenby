378f997f81539a654a56df2a9fb17d29286f2d2c
Monnorthegodlydev <141607683+Monnorthegodlydev@users.noreply.github.com>
2024-01-22 06:38:39 +0800

    Delete ConCon directory

--- ConCon/Connor Quiz Engine/clearterm.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def clear_terminal():
-    if os.name == 'posix':  
-        os.system('clear')
-    elif os.name == 'nt':  
-        os.system('cls')
-    else:
-        print("os not detected.")
